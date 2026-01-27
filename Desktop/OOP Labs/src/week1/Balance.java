4eba09e54d0ef8c0589fab3f4f1cdad2922de76b
AskarovaAkbota <askarovaakbota28@gmail.com>
2026-01-27 16:20:22 +0500

    Balance.java

--- Desktop/OOP Labs/src/week1/Balance.java
+++ Desktop/OOP Labs/src/week1/Balance.java
@@ -1,5 +1,21 @@
 package week1;
+import java.util.Scanner;
 
 public class Balance {
+
+	public static void main(String[] args) {
+		
+		int balance = 0;
+		
+	
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int money = scanner.nextInt();
+		balance += money;
+		
+		System.out.println("You inserted " + money + "KZT! Your current balance is " + balance);
+		
+	}
 
 }
